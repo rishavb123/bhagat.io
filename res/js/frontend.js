9d000970f7eaac4782105d37c4e36d7fa1ddefce
Rishav Bhagat <rishavb123@gmail.com>
2019-01-19 16:00:48 -0500

    got nav working well

--- res/js/frontend.js
+++ res/js/frontend.js
@@ -4,3 +4,21 @@ function headerImageRotate(a) {
     theta = a % 360;
     $('#header-img').css('transform', 'rotate(' + theta + 'deg)');
 }
+
+$('#about-me').waypoint(direction => {
+    console.log(direction);
+    if (direction === "down")
+        $('nav').addClass("fixed-top");
+    else
+        $('nav').removeClass("fixed-top");
+}, {
+    offset: "25%"
+});
+
+new SmoothScroll('a[href*="#"]', {
+    easing: "easeOutQuint",
+    speed: 2000,
+    durationMin: 1000,
+    updateURL: false,
+    popstate: true
+});
