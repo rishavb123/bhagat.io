3043d40953e682b9b7bb83e0742d02d5d03410b3
Rishav Bhagat <rishavb123@gmail.com>
2019-01-19 23:51:40 -0500

    made upgrades to ui and content

--- res/js/test.js
+++ res/js/test.js
@@ -1,3 +1,5 @@
 $.getJSON('https://ipapi.co/json/', function(data) {
     console.log(data);
 });
+
+// src: url(../fonts/Font-Name/Font-Name.woff) format("woff"), url(../fonts/Font-Name/Font-Name.woff2) format("woff2"), url(../fonts/Font-Name/Font-Name.ttf) format("ttf"), url(../fonts/Font-Name/Font-Name.svg) format("svg"), url(../fonts/Font-Name/Font-Name.otf) format("otf"), url(../fonts/Font-Name/Font-Name.eot) format("eot");
