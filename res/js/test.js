06a99be8a8211c8bab9a6b17954f1984db8f49c8
Rishav Bhagat <rishavb123@gmail.com>
2019-01-19 23:52:38 -0500

    minor changes

--- res/js/test.js
+++ res/js/test.js
@@ -1,5 +1,3 @@
 $.getJSON('https://ipapi.co/json/', function(data) {
-    console.log(data);
+    // console.log(data);
 });
-
-// src: url(../fonts/Font-Name/Font-Name.woff) format("woff"), url(../fonts/Font-Name/Font-Name.woff2) format("woff2"), url(../fonts/Font-Name/Font-Name.ttf) format("ttf"), url(../fonts/Font-Name/Font-Name.svg) format("svg"), url(../fonts/Font-Name/Font-Name.otf) format("otf"), url(../fonts/Font-Name/Font-Name.eot) format("eot");
