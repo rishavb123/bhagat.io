84f73fcdf2e21f4e58ffadd5b32ed092722bb447
Rishav Bhagat <rishavb123@gmail.com>
2019-04-24 09:05:56 -0400

    updated projects scrolling

--- res/js/frontend.js
+++ res/js/frontend.js
@@ -16,6 +16,38 @@ function copy(str) {
     alert("Copied " + str);
 };
 
+let pixels = 0;
+let projects = document.getElementById("full-project-list");
+
+function scrollProjects(num) {
+    let t = 0;
+    let origPixels = pixels;
+    let interval = setInterval(() => {
+        if(num > 0)
+            pixels += 2;
+        else
+            pixels -= 2;
+        projects.scroll(pixels, 0);
+        if(pixels - origPixels == num || pixels - origPixels == num + 1)
+            clearInterval(interval);
+        t++;
+        if(t > 500)
+            clearInterval(interval)
+    });
+}
+
+projects.onscroll = () => {
+    pixels = projects.scrollLeft;
+}
+
+document.getElementById("leftBtn").onclick = () => {
+    scrollProjects(document.getElementsByClassName("project")[0].clientWidth * -2);
+}
+
+document.getElementById("rightBtn").onclick = () => {
+    scrollProjects(document.getElementsByClassName("project")[0].clientWidth * 2);
+}
+
 $('.bring-in-sticky-nav').waypoint(direction => {
     if (direction === "down") {
         $('nav').css('display', "none");
