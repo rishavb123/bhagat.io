917e2ff13f6a05e4bdeb011156a1a28625ff6add
Rishav Bhagat <rishavb123@gmail.com>
2020-05-11 17:50:34 -0400

    removed video modifications and recording, also not loading video on mobile

--- res/js/overlay.js
+++ res/js/overlay.js
@@ -1,16 +1,16 @@
-const video = document.getElementsByTagName("video")[0];
+const video = document.getElementById("overlay_video");
 
 const _overlay = $('.overlay-canvas');
 const overlay = _overlay[0];
+
 const output = overlay.getContext('2d');
+
+const isTouchDevice = 'ontouchstart' in document.documentElement;
 
 let interval;
 
 overlay.width = innerWidth;
 overlay.height = innerHeight;
-
-// output.fillStyle = 'white';
-// output.fillRect(0, 0, 600, 600);
 
 addEventListener('resize', function() {
 	overlay.width = innerWidth;	
@@ -18,7 +18,6 @@ addEventListener('resize', function() {
 })
 
 function processFrame() {
-    console.log('processing frame');
     output.clearRect(0, 0, overlay.width, overlay.height);
 
     let x = 0,
@@ -27,45 +26,23 @@ function processFrame() {
         h = overlay.height;
 
     output.drawImage(video, x, y, w, h);
-
-    let	image = output.getImageData(0, 0, overlay.width, overlay.height),
-        imageData = image.data;
-
-        
-    for (let i = 0, len = imageData.length; i < len; i = i + 4) {
-        imageData[i + 3] = imageData[i] + imageData[i + 1] + imageData[i + 2] / 3;
-    }
-
-    output.putImageData(image, 0, 0);
 }
 
-// const myMediaSource = new MediaSource();
-// mediaSource.addEventListener('sourceopen', () => console.log("sJKSDL"));
-// const url = URL.createObjectURL(myMediaSource);
-// video.src = url;
-// const videoSourceBuffer = myMediaSource.addSourceBuffer('video/mp4; codecs="avc1.64001e"');
+if(!isTouchDevice) {
+    $('#karate').waypoint(direction => { 
+        if (direction === "down") {
+            video.play();
+            _overlay.fadeIn(1000);
 
-// fetch("https://bhagat.io/res/vids/karate_overlay_optimized.webm").then(function(response) {
-//     return response.arrayBuffer();
-// }).then(function(videoData) {
-//     videoSourceBuffer.appendBuffer(videoData);
-// });
-
-
-$('#karate').waypoint(direction => { 
-    console.log(direction);   
-    if (direction === "down") {
-        video.play();
-        _overlay.fadeIn(1000);
-
-    } else {
-        clearInterval(interval);
-        video.pause();
-        _overlay.fadeOut(2000);
-    }
-}, {
-    offset: "100%"
-});
+        } else {
+            clearInterval(interval);
+            video.pause();
+            _overlay.fadeOut(2000);
+        }
+    }, {
+        offset: "100%"
+    });
+}
 
 video.addEventListener('play', function() {
     clearInterval(interval);
