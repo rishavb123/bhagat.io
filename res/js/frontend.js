96631b44d6bde7264eab1a68d9b6de2238c8efca
Rishav Bhagat <rishavb123@gmail.com>
2020-05-11 20:22:55 -0400

    got overlay module system working

--- res/js/frontend.js
+++ res/js/frontend.js
@@ -68,7 +68,7 @@ for (let i in sections) {
     const section = sections[i];
     $("#" + section.id).waypoint(direction => {
         $(".nav-link").removeClass("active");
-        if (direction == "up")
+        if (direction == "up" && sections[i - 1])
             $("#nav-" + sections[i - 1].id).addClass("active");
         else
             $("#nav-" + section.id).addClass("active");
--- res/js/overlay.js
+++ res/js/overlay.js
@@ -1,16 +1,20 @@
 const video = document.getElementById("overlay_video");
 
 const _overlay = $('.overlay-canvas');
+_overlay.fadeOut(0);
 const overlay = _overlay[0];
 
 const output = overlay.getContext('2d');
 
 const isTouchDevice = 'ontouchstart' in document.documentElement;
 
-let interval;
+let overlay_state = "none";
 
 overlay.width = innerWidth;
 overlay.height = innerHeight;
+
+let education_img = new Image();
+education_img.src = "./res/imgs/overlay-images/equations.jpg";
 
 addEventListener('resize', function() {
 	overlay.width = innerWidth;	
@@ -25,28 +29,72 @@ function processFrame() {
         w = overlay.width,
         h = overlay.height;
 
-    output.drawImage(video, x, y, w, h);
+    switch(overlay_state) {
+        case "karate":
+            output.globalAlpha = 1;
+            output.drawImage(video, x, y, w, h);
+            break;
+        case "education":
+            output.globalAlpha = 0.1;
+            output.drawImage(education_img, x, y, w, h);
+            break;
+    }
 }
+
+overlay_config = {
+    "none": {},
+    "karate": {
+        offset: "65%",
+        onenter: () => video.play(),
+        onexit: () => video.pause(),
+        above: "education"
+    },
+    "education": {
+        offset: "20%",
+        above: "none"
+    }
+};
 
 if(!isTouchDevice) {
-    $('#karate').waypoint(direction => { 
-        if (direction === "down") {
-            video.play();
-            _overlay.fadeIn(1000);
-
-        } else {
-            clearInterval(interval);
-            video.pause();
-            _overlay.fadeOut(2000);
-        }
-    }, {
-        offset: "75%"
-    });
+    for(let overlay_section in overlay_config) {
+        if(overlay_section == 'none') continue;
+        let overlay_section_config = overlay_config[overlay_section];
+        $(`#${overlay_section}`).waypoint(direction => {
+            if(direction == "down") {
+                console.log(overlay_section, overlay_section_config);
+                if(overlay_section_config.onenter)
+                    overlay_section_config.onenter();
+                let t = 0;
+                if(overlay_state !== "none") {
+                    _overlay.fadeOut(1000);
+                    t = 1000;
+                }
+                setTimeout(() => {
+                    if(overlay_config[overlay_state].onexit)
+                        overlay_config[overlay_state].onexit();
+                    overlay_state = overlay_section;
+                    _overlay.fadeIn(1000);
+                    console.log("hi");
+                }, t);
+            } else {
+                if(overlay_section_config.onexit)
+                    overlay_section_config.onexit();
+                _overlay.fadeOut(1000);
+                setTimeout(() => {
+                    overlay_state = overlay_section_config.above;
+                    if(overlay_config[overlay_state].onenter)
+                        overlay_config[overlay_state].onenter();
+                    if(overlay_state !== "none") {
+                        _overlay.fadeIn(1000);
+                    }
+                }, 1000);
+            }
+        }, {
+            offset: overlay_section_config.offset
+        });
+    }
 }
 
-video.addEventListener('play', function() {
-    clearInterval(interval);
-    interval = setInterval(processFrame, 40);
-}, false);
-
 video.onended = () => video.play();
+
+let interval = setInterval(processFrame, 40);;
