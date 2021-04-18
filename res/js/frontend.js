76263200cbf1fb2a7db394cc68ebdccb4941d6a1
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2021-04-17 21:05:40 -0400

    switched to image of setup

--- res/js/frontend.js
+++ res/js/frontend.js
@@ -95,21 +95,21 @@ if('ontouchstart' in document.documentElement) {
     $('#setup-video').attr("src", "https://www.youtube.com/embed/XuCRpQ7TmHo?rel=0&controls=1")
 }
 
-let player;
-function onYouTubeIframeAPIReady() {
-  player = new YT.Player('setup-video', {
-    events: {
-      'onReady': onPlayerReady,
-      'onStateChange': onPlayerStateChange
-    }
-  });
-}
+// let player;
+// function onYouTubeIframeAPIReady() {
+//   player = new YT.Player('setup-video', {
+//     events: {
+//       'onReady': onPlayerReady,
+//       'onStateChange': onPlayerStateChange
+//     }
+//   });
+// }
 
-function onPlayerReady(event) {
-    event.target.playVideo();
-}
+// function onPlayerReady(event) {
+//     event.target.playVideo();
+// }
 
-function onPlayerStateChange(event) {
-    if(event.data == YT.PlayerState.ENDED)
-        event.target.playVideo();
-}
+// function onPlayerStateChange(event) {
+//     if(event.data == YT.PlayerState.ENDED)
+//         event.target.playVideo();
+// }
