fd0a21dd262b92ed5014b3cb5116e68bb6dd2fe1
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2022-11-25 02:32:10 -0500

    switched to on render service

--- res/js/constants.js
+++ res/js/constants.js
@@ -1,4 +1,4 @@
 const DEV_API_URL = 'http://localhost:3000';
-const PROD_API_URL = 'https://bhagat-api.herokuapp.com';
+const PROD_API_URL = 'https://bhagat-api.onrender.com';
 
 const API_URL = (!location.href.includes('dev'))? PROD_API_URL: DEV_API_URL;
