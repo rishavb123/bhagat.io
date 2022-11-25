24ad00a3ad506c1f351bdd862af91d0b6ad1a879
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2022-11-25 02:39:10 -0500

    update prod api url

--- res/js/constants.js
+++ res/js/constants.js
@@ -1,4 +1,5 @@
 const DEV_API_URL = 'http://localhost:3000';
-const PROD_API_URL = 'https://bhagat-api.onrender.com';
+// const PROD_API_URL = 'https://bhagat-api.onrender.com';
+const PROD_API_URL = 'https://api.bhagat.io';
 
 const API_URL = (!location.href.includes('dev'))? PROD_API_URL: DEV_API_URL;
